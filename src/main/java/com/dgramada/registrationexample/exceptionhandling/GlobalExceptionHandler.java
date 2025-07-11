41fd88997ab93ce43f6905b3b297d970ae8c7812
Yordan <yordan.tsintsov@nevexis.com>
2025-07-11 14:39:39 +0300

    refactored ControllerAdvice to @RestControllerAdvice

--- src/main/java/com/dgramada/registrationexample/exceptionhandling/GlobalExceptionHandler.java
+++ src/main/java/com/dgramada/registrationexample/exceptionhandling/GlobalExceptionHandler.java
@@ -4,35 +4,35 @@ import com.dgramada.registrationexample.exceptionhandling.exception.ResourceAlre
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse exceptionHandler(Exception ex, WebRequest request) {
         logger.error("An unexpected error occurred: ", ex);
-        final var errorResponse = new ErrorResponse(
+        return new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                "Internal server error",
                 "An unexpected error occurred. Please try again later.",
                 request.getDescription(false).replace("uri=", "")
         );
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex, WebRequest request) {
         logger.warn("Invalid method arguments: ", ex);
         Map<String, String> validationErrors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach((fieldError) ->
@@ -41,25 +41,25 @@ public class GlobalExceptionHandler {
 
         final var errorResponse = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.toString(),
+                "Bad request",
                 "Invalid data provided. Please check the request.",
                 request.getDescription(false).replace("uri=", "")
         );
         errorResponse.setValidationErrors(validationErrors);
 
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return errorResponse;
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> resourceAlreadyExistsExceptionHandler(ResourceAlreadyExistsException ex, WebRequest request) {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse resourceAlreadyExistsExceptionHandler(ResourceAlreadyExistsException ex, WebRequest request) {
         logger.warn("Resource already exists: ", ex);
-        final var errorResponse = new ErrorResponse(
+        return new ErrorResponse(
                 HttpStatus.CONFLICT.value(),
-                HttpStatus.CONFLICT.toString(),
+                "Conflict",
                 ex.getMessage(),
                 request.getDescription(false).replace("uri=", "")
         );
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
